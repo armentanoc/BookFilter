0285399a8d3a2ab31c023e36fcaa722da3857ca1
Carolina Armentano <armentanocarolina@gmail.com>
2024-01-31 11:03:53 -0300

    chore: remove unnecessary comments.

--- BookFilter.UI/Program.cs
+++ BookFilter.UI/Program.cs
@@ -17,29 +17,3 @@ namespace BookFilter.UI
         }
     }
 }
-
-//old code, for comparison only
-
-//exemplo
-
-//public List<int> FilterNumber(Func<int, bool> filter)
-//{
-//    return _numbers.Where(filter).ToList();
-//}
-
-// EXEMPLO: FilterNumber(number => number % 2 == 0)
-// RETORNO: todos os pares da lista
-
-
-
-//Func<Livro, bool> funcAnoPublicacao = livro => LivroFilter.AnoPublicacaoMaiorOuIgual(livro, ANO_PUBLICACAO_MAXIMO);
-//List<Livro> resultadoAnoPublicacaoFunc = livroRepository.FiltrarComFunc(funcAnoPublicacao);
-////preferível
-
-//Filter<Livro> filtroPrecoMaximo = livro => LivroFilter.PrecoMenorOuIgual(livro, PRECO_MAXIMO);
-//List<Livro> resultadoPrecoMaximo = livroRepository.Filtrar(filtroPrecoMaximo);
-////é bom porque mantém genérico 
-
-//Func<Livro, string, bool> filtroPalavraNoTitulo = (livro, palavra) => livro.Titulo.Contains(palavra, StringComparison.OrdinalIgnoreCase);
-//List<Livro> resultadoPalavraNoTitulo = livroRepository.FiltrarDiretamente(filtroPalavraNoTitulo, "C#");
-////ruim porque mistura muito as coisas e não é tão genérico quanto gostaríamos
