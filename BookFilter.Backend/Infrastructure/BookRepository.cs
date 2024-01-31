e4a52f52a76630b814f09d079db8d5384b76d5ab
Carolina Armentano <armentanocarolina@gmail.com>
2024-01-31 11:06:06 -0300

    chore: remove unnecessary comments.

--- BookFilter.Backend/Infrastructure/BookRepository.cs
+++ BookFilter.Backend/Infrastructure/BookRepository.cs
@@ -13,7 +13,7 @@ namespace BookFilter.Backend.Infrastructure
             _books = books;
         }
 
-        public List<Book> Filter(IFilter<Book> iFilter) //interface filter
+        public List<Book> Filter(IFilter<Book> iFilter) 
         {
             return _books.Where(x => iFilter.Filter(x)).ToList();
         }
@@ -24,26 +24,3 @@ namespace BookFilter.Backend.Infrastructure
         }
     }
 }
-
-//Old code, for comparison only
-
-//public Func<Book, bool> booleanBookFilter;
-
-//public List<Book> FuncFilter(Func<Book, bool> filter)
-//{
-//    return _books.Where(x => filter(x)).ToList();
-//}
-
-//public List<Book> DirectFilter(Func<Book, string, bool> filter, string givenWord)
-//{
-//    return _books.Where(livro => filter(livro, givenWord)).ToList();
-//}
-
-//public List<Livro> booleanBooks(Func<Livro, bool> booleanBookFilter)
-//{
-//    return livros.Where(booleanBookFilter).ToList();
-//}
-//public List<Livro> Filtrar(Filter<Livro> filtro)
-//{
-//    return livros.Where(x => filtro(x)).ToList();
-//}
